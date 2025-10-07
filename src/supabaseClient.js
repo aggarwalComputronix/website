a3b12b3ff2699dd6df98205c5ed34318bdfd823c
Santhosh Kunam <kunamsanthosh992@gmail.com>
2025-10-07 16:36:53 +0530

    All set for initial deployment stage

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Replace these with your actual Supabase project URL and public key
-const supabaseUrl = 'https://iwcrpoekwcoozximlcpg.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml3Y3Jwb2Vrd2Nvb3p4aW1sY3BnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkwNjExMjUsImV4cCI6MjA3NDYzNzEyNX0.v4XsWWYIK8mJo372dxJfqyZxlZxaIlEUa4rYnBMdiYA';
+// Netlify will securely inject these variables during the build process.
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
